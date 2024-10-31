f41a801e0efdd3ea3bdc8bc9d96bd9450a308224
howard.wang <howard.wang@bytedance.com>
2024-10-31 09:23:08 +0000

    implement manusal screen save_frontier WIP

--- bayesian/bayesian.py
+++ bayesian/bayesian.py
@@ -128,6 +128,48 @@ def plot_frontier(frontier):
     pass
 
 
+def save_experiment_and_frontier(experiment, frontier, path='pareto_frontier.json', manual=False, components=None):
+    raw_param_dicts = frontier.param_dicts
+    if manual:
+        def calculate_trial_index(components, parameters):
+            return parameters['photosensitizer'] * len(components['solvent']) + parameters['solvent']
+
+        param_dicts = []
+        trial_dicts = []
+        for i in range(len(raw_param_dicts)):
+            param_data = {
+                'photosensitizer': components['photosensitizer'][raw_param_dicts[i]['photosensitizer']],
+                'solvent': components['solvent'][raw_param_dicts[i]['solvent']],
+                }
+            param_dicts.append(param_data)
+            trial_data = experiment.fetch_trials_data_results(trial_indices=[calculate_trial_index(components, raw_param_dicts[i])],
+                                                              metrics=experiment.metrics.values()).values()
+            trial_data = list(trial_data)[0]
+
+            trial_data = {
+                'phi_singlet_oxygen': trial_data['phi_singlet_oxygen'].value.df['mean'][0], 
+                'max_absorption': trial_data['max_absorption'].value.df['mean'][0]
+            }
+            trial_dicts.append(trial_data)
+    else:
+        param_dicts = raw_param_dicts
+        for i in range(len(raw_param_dicts)):
+            trial_data = experiment.fetch_trials_data_results(trial_indices=[calculate_trial_index(components, raw_param_dicts[i])],
+                                                              metrics=experiment.metrics.values()).values()
+            trial_data = list(trial_data)[0]
+
+            trial_data = {
+                'phi_singlet_oxygen': trial_data['phi_singlet_oxygen'].value.df['mean'][0], 
+                'max_absorption': trial_data['max_absorption'].value.df['mean'][0]
+            }
+            trial_dicts.append(trial_data)
+        raise NotImplementedError('trial data not implemented')
+
+    # print(trial_dicts, param_dicts)
+    with open(path, 'w') as f:
+        json.dump({'param_dicts': param_dicts, 'trial_dicts': trial_dicts}, f)
+
+
 def screen(parameter_list: list,
            objectives: dict,
            predictor: BayesianPredictor,
@@ -167,6 +209,59 @@ def screen(parameter_list: list,
         plot_frontier(frontier)
 
     return ax_client, eval_results, frontier
+
+
+def manual_screen(generations: list,
+           objectives: dict,
+        #    predictor: BayesianPredictor,
+           plot: bool = False,
+           num_point: int = 20):
+    ax_client = AxClient()
+    ax_client.create_experiment(
+        name="screen_photosensitizer_solution",
+        parameters=[
+            {
+                "name": 'photosensitizer',
+                "type": "range",
+                "bounds": [-1, len(generations)+1],
+            }
+        ],
+        objectives=objectives,
+        overwrite_existing_experiment=True,
+        is_test=True,
+    )
+    eval_results = []
+
+    trial_index = 0
+    # manually screen photosensitizer and solvent
+    for i in range(len(generations)):
+        parameters_trial = [{
+            'photosensitizer': i,
+        }]
+        print(parameters_trial)
+        ax_client.experiment.attach_trial(parameters_trial)
+        # Local evaluation here can be replaced with deployment to external system.
+        results = {
+            "phi_singlet_oxygen": tuple(generations[i]['phi_singlet_oxygen']), 
+            "max_absorption": tuple(generations[i]['max_absorption']),
+            }
+        ax_client.complete_trial(trial_index=trial_index, raw_data=results)
+        trial_index += 1
+
+    ax_objectives = ax_client.experiment.optimization_config.objective.objectives
+    frontier = compute_posterior_pareto_frontier(
+        experiment=ax_client.experiment,
+        data=ax_client.experiment.fetch_data(),
+        primary_objective=ax_objectives[1].metric,
+        secondary_objective=ax_objectives[0].metric,
+        absolute_metrics=['phi_singlet_oxygen', 'max_absorption'],
+        num_points=num_point,
+    )
+
+    if plot:
+        save_experiment_and_frontier(ax_client.experiment, frontier, manual=True, generations=generations, path='/mlx_devbox/users/howard.wang/playground/molllm/datasets/pareto_frontier.json')
+
+    return ax_client.experiment
 
 
 def main():
